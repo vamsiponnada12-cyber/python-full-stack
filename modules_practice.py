ddee33eb410817167885fc760040f0cab5a5f6cb
vansiponnada12-cyber <vamsiponnada12@gmail.com>
2026-09-07 20:07:47 +0530

    initial files

---
+++ modules_practice.py
@@ -0,0 +1,6 @@
+def add_(a,b):
+    return(a+b)
+def sub_(a,b):
+    return(a-b)
+def pw(a,b):
+    return(a**b)
---
+++ practice.py
@@ -0,0 +1,17 @@
+'''
+
+num = int(input('Entre a number: '))
+if num % 2 == 0:
+    print(f'{num} is even')
+else:
+    print(f'{num} is odd')
+
+
+l = int(input('entre a lengeth: '))
+w = int(input('enter a width: '))
+calc_ =(l*w)
+print('area of rectangle: ',calc_)
+'''
+name = input('Enter a name: ')
+age = int(input('Enter your age: '))
+print(f'hello {name}',f'{age} is fantastic')
